085b4a9a914ab8bc038ed670052b59950954586e
Valdemar Jr <valdemar.barros@livelo.com.br>
2024-02-24 13:48:50 -0300

    added web and testcontainers project examples

---
+++ test-container/src/test/java/br/com/valdemarjr/testcontainer/TestContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdemarjr.testcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ test-container/src/test/java/br/com/valdemarjr/testcontainer/TestTestContainerApplication.java
@@ -0,0 +1,60 @@
+package br.com.valdemarjr.testcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+/** Test application to integrate with TestContainers for local development */
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTestContainerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(TestContainerApplication::main)
+        .with(TestTestContainerApplication.class)
+        .run(args);
+  }
+
+  @Bean
+  @RestartScope
+  // springboot-devtools must be in test scope. This annotation will keep the container up and
+  // running even if the application restarts
+  @ServiceConnection
+  PostgreSQLContainer postgresSQLContainer() {
+    return new PostgreSQLContainer("postgres");
+  }
+
+  @Container
+  static GenericContainer container =
+      new GenericContainer(DockerImageName.parse("myImage/myImage"))
+          .withEnv("my.env", "value")
+          .withExposedPorts(9999)
+          .withNetworkAliases("myNetworkAlias")
+          .withCommand("echo Hello World");
+
+  @DynamicPropertySource
+  public static void myProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.mail.host", container::getHost);
+    registry.add("spring.mail.port", container::getFirstMappedPort);
+  }
+
+  @Bean
+  @RestartScope
+  // springboot-devtools must be in test scope. This annotation will keep the container up and
+  // running even if the application restarts
+  @ServiceConnection
+  GenericContainer myContainer() {
+    return new GenericContainer(DockerImageName.parse("myImage/myImage"))
+        .withEnv("my.env", "value")
+        .withExposedPorts(9999)
+        .withNetworkAliases("myNetworkAlias")
+        .withCommand("echo Hello World");
+  }
+}
